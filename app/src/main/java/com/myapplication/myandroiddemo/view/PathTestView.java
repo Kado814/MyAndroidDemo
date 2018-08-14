55199fde323f2736512b86cd77a51b66f6222672
kangzj <kangzj@live.dd>
2018-08-14 16:58:02 +0800

    11
    1

---
+++ app/src/main/java/com/myapplication/myandroiddemo/view/PathTestView.java
@@ -0,0 +1,187 @@
+package com.myapplication.myandroiddemo.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by ${KZJ} on 2018/7/23.
+ * Android雷达图(蜘蛛网图)绘制
+ */
+
+public class PathTestView extends View {
+    private int count = 6;                //数据个数
+    private float angle = (float) (Math.PI * 2 / count);
+    private float radius;                   //网格最大半径
+    private int centerX;                  //中心X
+    private int centerY;                  //中心Y
+    private String[] titles = {"a", "b", "c", "d", "e", "f"};
+    private double[] data = {60, 100, 60, 100, 90, 50, 10, 20}; //各维度分值
+    private float maxValue = 100;             //数据最大值
+    private Paint mainPaint;                //雷达区画笔
+    private Paint valuePaint;               //数据区画笔
+    private Paint textPaint;                //文本画笔
+
+    public PathTestView(Context context) {
+        super(context);
+    }
+
+    public PathTestView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PathTestView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public PathTestView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        radius = Math.min(h, w) / 2 * 0.9f;
+        centerX = w / 2;
+        centerY = h / 2;
+        postInvalidate();
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        //        Paint paint = new Paint();
+        //        paint.setColor(Color.BLACK);
+        //        paint.setStyle(Paint.Style.STROKE);//填充模式，描边
+        //        paint.setStrokeWidth(10);//边框宽度
+        //
+        //        canvas.translate(getWidth() / 2, getHeight() / 2);
+        //        Path path = new Path();
+        //        path.lineTo(200, 200);
+        ////        path.moveTo(200,100);//移动下一次操作的起点位置
+        //        path.setLastPoint(200,100);//设置之前操作的最后一个点位置
+        //        path.lineTo(200, 0);
+        //
+        //        //close方法用于连接当前最后一个点和最初的一个点(如果两个点不重合的话)，最终形成一个封闭的图形,
+        //        // 如果连接了最后一个点和第一个点仍然无法形成封闭图形，则close什么 也不做。
+        //        path.close();
+        //        canvas.drawPath(path, paint);
+        mainPaint = new Paint();
+        mainPaint.setColor(Color.BLACK);
+        mainPaint.setStyle(Paint.Style.STROKE);
+        mainPaint.setStrokeWidth(1);
+
+        textPaint = new Paint();
+        textPaint.setColor(Color.BLACK);
+        textPaint.setStyle(Paint.Style.STROKE);
+        textPaint.setStrokeWidth(1);
+
+        valuePaint = new Paint();
+        valuePaint.setColor(Color.BLUE);
+        valuePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        //        valuePaint.setStrokeWidth(1);
+
+        drawPolygon(canvas);
+        drawLines(canvas);
+        drawText(canvas);
+        drawRegion(canvas);
+    }
+
+    /**
+     * 绘制正多边形
+     */
+    private void drawPolygon(Canvas canvas) {
+        Path path = new Path();
+        float r = radius / (count - 1);//r是蜘蛛丝之间的间距
+        for (int i = 1; i < count; i++) {//中心点不用绘制
+            float curR = r * i;//当前半径
+            path.reset();
+            for (int j = 0; j < count; j++) {
+                if (j == 0) {
+                    path.moveTo(centerX + curR, centerY);
+                } else {
+                    //根据半径，计算出蜘蛛丝上每个点的坐标
+                    float x = (float) (centerX + curR * Math.cos(angle * j));
+                    float y = (float) (centerY + curR * Math.sin(angle * j));
+                    path.lineTo(x, y);
+                }
+            }
+            path.close();//闭合路径
+            canvas.drawPath(path, mainPaint);
+        }
+    }
+
+    /**
+     * 绘制直线
+     */
+    private void drawLines(Canvas canvas) {
+        Path path = new Path();
+        for (int i = 0; i < count; i++) {
+            path.reset();
+            path.moveTo(centerX, centerY);
+            float x = (float) (centerX + radius * Math.cos(angle * i));
+            float y = (float) (centerY + radius * Math.sin(angle * i));
+            path.lineTo(x, y);
+            canvas.drawPath(path, mainPaint);
+        }
+    }
+
+    /**
+     * 绘制文字
+     *
+     * @param canvas
+     */
+    private void drawText(Canvas canvas) {
+        Paint.FontMetrics fontMetrics = textPaint.getFontMetrics();
+        float fontHeight = fontMetrics.descent - fontMetrics.ascent;
+        for (int i = 0; i < count; i++) {
+            float x = (float) (centerX + (radius + fontHeight / 2) * Math.cos(angle * i));
+            float y = (float) (centerY + (radius + fontHeight / 2) * Math.sin(angle * i));
+            if (angle * i >= 0 && angle * i <= Math.PI / 2) {//第4象限
+                canvas.drawText(titles[i], x, y, textPaint);
+            } else if (angle * i >= 3 * Math.PI / 2 && angle * i <= Math.PI * 2) {//第3象限
+                canvas.drawText(titles[i], x, y, textPaint);
+            } else if (angle * i > Math.PI / 2 && angle * i <= Math.PI) {//第2象限
+                float dis = textPaint.measureText(titles[i]);//文本长度
+                canvas.drawText(titles[i], x - dis, y, textPaint);
+            } else if (angle * i >= Math.PI && angle * i < 3 * Math.PI / 2) {//第1象限
+                float dis = textPaint.measureText(titles[i]);//文本长度
+                canvas.drawText(titles[i], x - dis, y, textPaint);
+            }
+        }
+    }
+
+    /**
+     * 绘制区域
+     *
+     * @param canvas
+     */
+    private void drawRegion(Canvas canvas) {
+        Path path = new Path();
+        valuePaint.setAlpha(255);
+        for (int i = 0; i < count; i++) {
+            double percent = data[i] / maxValue;
+            float x = (float) (centerX + radius * Math.cos(angle * i) * percent);
+            float y = (float) (centerY + radius * Math.sin(angle * i) * percent);
+            if (i == 0) {
+                path.moveTo(x, centerY);
+            } else {
+                path.lineTo(x, y);
+            }
+            //绘制小圆点
+            canvas.drawCircle(x, y, 10, valuePaint);
+        }
+        valuePaint.setStyle(Paint.Style.STROKE);
+        canvas.drawPath(path, valuePaint);
+        valuePaint.setAlpha(127);
+        //绘制填充区域
+        valuePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        canvas.drawPath(path, valuePaint);
+    }
+}
